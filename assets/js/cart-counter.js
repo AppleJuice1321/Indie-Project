da320d63152c32e330befa8ee0d437186ea7ea1f
AppleJuice1321 <rafaelgeisler6@gmail.com>
2023-06-02 14:03:46 +0200

    tweaks

---
+++ assets/js/cart-counter.js
@@ -0,0 +1,10 @@
+(function () {
+    let button = document.querySelector(".headsets__button")
+    let counter = document.querySelector("cart__counter")
+    
+    button.addEventListener("click", increaseCounter)
+
+    function increaseCounter() {
+        button
+    }
+})()
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 import "./assets/js/login.js"
 import "./assets/js/cards.js"
 import "./assets/js/slideshow.js"
+import "./assets/js/cart-counter.js"
